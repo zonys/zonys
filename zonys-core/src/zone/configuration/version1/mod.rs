17decedf1afdea32924e07ab5929b959f4c7163f
Justus Flerlage <justus@sutsuj.com>
2022-06-08 07:21:30 +0200

    Add tags configuration directive

--- zonys-core/src/zone/configuration/version1/mod.rs
+++ zonys-core/src/zone/configuration/version1/mod.rs
@@ -27,6 +27,7 @@ impl Default for Version1ZoneConfigurationType {
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Version1ZoneConfiguration {
     variables: Option<TemplateObject>,
+    tags: Option<Vec<String>>,
     #[serde(flatten)]
     r#type: Version1ZoneConfigurationType,
     start_after_create: Option<bool>,
@@ -36,12 +37,14 @@ pub struct Version1ZoneConfiguration {
 impl Version1ZoneConfiguration {
     pub fn new(
         variables: Option<TemplateObject>,
+        tags: Option<Vec<String>>,
         r#type: Version1ZoneConfigurationType,
         start_after_create: Option<bool>,
         destroy_after_stop: Option<bool>,
     ) -> Self {
         Self {
             variables,
+            tags,
             r#type,
             start_after_create,
             destroy_after_stop,
@@ -58,6 +61,18 @@ impl Version1ZoneConfiguration {
 
     pub fn set_variables(&mut self, variables: Option<TemplateObject>) {
         self.variables = variables
+    }
+
+    pub fn tags(&self) -> &Option<Vec<String>> {
+        &self.tags
+    }
+
+    pub fn tags_mut(&mut self) -> &mut Option<Vec<String>> {
+        &mut self.tags
+    }
+
+    pub fn set_tags(&mut self, tags: Option<Vec<String>>) {
+        self.tags = tags
     }
 
     pub fn r#type(&self) -> &Version1ZoneConfigurationType {
