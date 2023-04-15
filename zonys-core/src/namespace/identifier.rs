78f549e08767dc05ae1b1d507a317437f4d92d6d
Justus Flerlage <justus@sutsuj.com>
2023-04-15 21:30:22 +0200

    Fix NamespaceIdentifier PathBuf conversion

--- zonys-core/src/namespace/identifier.rs
+++ zonys-core/src/namespace/identifier.rs
@@ -1,9 +1,9 @@
-use super::error::ConvertNamespaceIdentifierFromStrError;
 use serde::{Deserialize, Serialize};
-use std::fmt;
 use std::fmt::{Display, Formatter};
-use std::path::PathBuf;
+use std::fmt;
+use std::path::{PathBuf, MAIN_SEPARATOR_STR};
 use std::str::FromStr;
+use super::error::ConvertNamespaceIdentifierFromStrError;
 use zfs::file_system::identifier::FileSystemIdentifier;
 use zfs::pool::identifier::PoolIdentifier;
 use ztd::{Constructor, Method};
@@ -72,11 +72,12 @@ impl From<NamespaceIdentifier> for PathBuf {
     fn from(identifier: NamespaceIdentifier) -> Self {
         let mut path = Self::with_capacity(identifier.child_components.len() + 1);
 
+        path.push(MAIN_SEPARATOR_STR);
+        path.push(identifier.root_component);
+
         for child_component in identifier.child_components {
             path.push(child_component);
         }
-
-        path.push(identifier.root_component);
 
         path
     }
