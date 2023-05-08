4aa3ca80590965cee1ad79c04c9b94498a544a74
Justus Flerlage <justus@sutsuj.com>
2023-05-09 00:44:59 +0200

    Reimplement jail command execution

--- zonys-core/src/configuration/reader/jail.rs
+++ zonys-core/src/configuration/reader/jail.rs
@@ -1,8 +1,32 @@
 use crate::{
-    ZoneConfigurationReaderTraverser, ZoneConfigurationUnit, ZoneConfigurationVersion1TypeUnit,
+    ZoneConfigurationReaderTraverser, ZoneConfigurationUnit,
+    ZoneConfigurationVersion1JailProgramUnit, ZoneConfigurationVersion1TypeUnit,
     ZoneConfigurationVersion1VolumeUnit, ZoneConfigurationVersionUnit,
 };
-use ztd::Constructor;
+use std::collections::HashMap;
+use std::iter::empty;
+use ztd::{Constructor, Method};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Constructor, Debug, Method)]
+#[Method(accessors)]
+#[Constructor(visibility = pub(self))]
+pub struct JailZoneConfigurationStep<'a> {
+    program: &'a String,
+    arguments: &'a Option<Vec<String>>,
+    environment_variables: &'a Option<HashMap<String, String>>,
+}
+
+impl<'a> From<&'a ZoneConfigurationVersion1JailProgramUnit> for JailZoneConfigurationStep<'a> {
+    fn from(unit: &'a ZoneConfigurationVersion1JailProgramUnit) -> Self {
+        Self::new(
+            unit.program(),
+            unit.arguments(),
+            unit.environment_variables(),
+        )
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -65,5 +89,215 @@ impl<'a> JailZoneConfigurationReader<'a> {
         }
 
         None
+    }
+
+    pub fn create_steps(&self) -> impl Iterator<Item = JailZoneConfigurationStep<'a>> {
+        ZoneConfigurationReaderTraverser::new(vec![self.unit])
+            .inorder()
+            .flat_map(|unit| match unit.version() {
+                ZoneConfigurationVersionUnit::Version1(version1) => {
+                    let jail = match version1.r#type() {
+                        ZoneConfigurationVersion1TypeUnit::Jail(jail) => jail,
+                    };
+
+                    let execute = match jail.execute() {
+                        Some(execute) => execute,
+                        None => return None,
+                    };
+
+                    let create = match execute.create() {
+                        Some(create) => create,
+                        None => return None,
+                    };
+
+                    let on = match create.on() {
+                        Some(on) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(on.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    let after = match create.after() {
+                        Some(after) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(after.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    Some(on.chain(after))
+                }
+            })
+            .flatten()
+    }
+
+    pub fn start_steps(&self) -> impl Iterator<Item = JailZoneConfigurationStep<'a>> {
+        ZoneConfigurationReaderTraverser::new(vec![self.unit])
+            .inorder()
+            .flat_map(|unit| match unit.version() {
+                ZoneConfigurationVersionUnit::Version1(version1) => {
+                    let jail = match version1.r#type() {
+                        ZoneConfigurationVersion1TypeUnit::Jail(jail) => jail,
+                    };
+
+                    let execute = match jail.execute() {
+                        Some(execute) => execute,
+                        None => return None,
+                    };
+
+                    let start = match execute.start() {
+                        Some(start) => start,
+                        None => return None,
+                    };
+
+                    let before = match start.before() {
+                        Some(before) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(before.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    let on = match start.on() {
+                        Some(on) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(on.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    let after = match start.after() {
+                        Some(after) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(after.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    Some(before.chain(on).chain(after))
+                }
+            })
+            .flatten()
+    }
+
+    pub fn stop_steps(&self) -> impl Iterator<Item = JailZoneConfigurationStep<'a>> {
+        ZoneConfigurationReaderTraverser::new(vec![self.unit])
+            .inorder()
+            .flat_map(|unit| match unit.version() {
+                ZoneConfigurationVersionUnit::Version1(version1) => {
+                    let jail = match version1.r#type() {
+                        ZoneConfigurationVersion1TypeUnit::Jail(jail) => jail,
+                    };
+
+                    let execute = match jail.execute() {
+                        Some(execute) => execute,
+                        None => return None,
+                    };
+
+                    let stop = match execute.stop() {
+                        Some(stop) => stop,
+                        None => return None,
+                    };
+
+                    let before = match stop.before() {
+                        Some(before) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(before.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    let on = match stop.on() {
+                        Some(on) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(on.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    let after = match stop.after() {
+                        Some(after) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(after.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    Some(before.chain(on).chain(after))
+                }
+            })
+            .flatten()
+    }
+
+    pub fn destroy_steps(&self) -> impl Iterator<Item = JailZoneConfigurationStep<'a>> {
+        ZoneConfigurationReaderTraverser::new(vec![self.unit])
+            .inorder()
+            .flat_map(|unit| match unit.version() {
+                ZoneConfigurationVersionUnit::Version1(version1) => {
+                    let jail = match version1.r#type() {
+                        ZoneConfigurationVersion1TypeUnit::Jail(jail) => jail,
+                    };
+
+                    let execute = match jail.execute() {
+                        Some(execute) => execute,
+                        None => return None,
+                    };
+
+                    let destroy = match execute.destroy() {
+                        Some(destroy) => destroy,
+                        None => return None,
+                    };
+
+                    let before = match destroy.before() {
+                        Some(before) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(before.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    let on = match destroy.on() {
+                        Some(on) => {
+                            Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                                Box::new(on.iter().map(JailZoneConfigurationStep::from)),
+                            )
+                        }
+                        None => Box::<dyn Iterator<Item = JailZoneConfigurationStep<'a>>>::from(
+                            Box::new(empty()),
+                        ),
+                    };
+
+                    Some(before.chain(on))
+                }
+            })
+            .flatten()
     }
 }
--- zonys-core/src/type/jail.rs
+++ zonys-core/src/type/jail.rs
@@ -1,9 +1,15 @@
 use crate::{
     CleanupZoneVolumeError, CreateZoneVolumeError, DestroyZoneVolumeError, FromHandler,
-    FromHandlerError, JailZoneConfigurationVolumeType, ReadZoneConfigurationError,
-    ReceiveZoneVolumeError, SendZoneVolumeError, Zone, ZoneConfigurationTypeReader,
-    ZoneDirectoryVolume, ZoneTransmissionReader, ZoneTransmissionWriter, ZoneVolume, ZoneZfsVolume,
+    FromHandlerError, JailZoneConfigurationStep, JailZoneConfigurationVolumeType,
+    ReadZoneConfigurationError, ReceiveZoneVolumeError, SendZoneVolumeError, Zone,
+    ZoneConfigurationTypeReader, ZoneDirectoryVolume, ZoneTransmissionReader,
+    ZoneTransmissionWriter, ZoneVolume, ZoneZfsVolume,
 };
+use jail::{
+    CreateJailError, DestroyJailError, ExecuteJailError, Jail, JailId, JailName, JailParameter,
+    TryIntoJailIdError,
+};
+use std::path::PathBuf;
 use ztd::{Constructor, Display, Error, From};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -14,6 +20,9 @@ pub enum CreateJailZoneError {
     ReadZoneConfigurationError(ReadZoneConfigurationError),
     CreateZoneVolumeError(CreateZoneVolumeError),
     FromHandlerError(FromHandlerError),
+    CreateJailError(CreateJailError),
+    DestroyJailError(DestroyJailError),
+    ExecuteJailError(ExecuteJailError),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,7 +30,12 @@ pub enum CreateJailZoneError {
 #[derive(Debug, Display, Error, From)]
 #[From(unnamed)]
 pub enum StartJailZoneError {
-    Unknown,
+    ReadZoneConfigurationError(ReadZoneConfigurationError),
+    CreateJailError(CreateJailError),
+    ExecuteJailError(ExecuteJailError),
+    #[Display("Jail is already running")]
+    AlreadyRunning,
+    TryIntoJailIdError(TryIntoJailIdError),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,7 +43,12 @@ pub enum StartJailZoneError {
 #[derive(Debug, Display, Error, From)]
 #[From(unnamed)]
 pub enum StopJailZoneError {
-    Unknown,
+    ReadZoneConfigurationError(ReadZoneConfigurationError),
+    DestroyJailError(DestroyJailError),
+    ExecuteJailError(ExecuteJailError),
+    #[Display("Jail is not running")]
+    NotRunning,
+    TryIntoJailIdError(TryIntoJailIdError),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,6 +58,12 @@ pub enum StopJailZoneError {
 pub enum DestroyJailZoneError {
     ReadZoneConfigurationError(ReadZoneConfigurationError),
     DestroyZoneVolumeError(DestroyZoneVolumeError),
+    CreateJailError(CreateJailError),
+    DestroyJailError(DestroyJailError),
+    ExecuteJailError(ExecuteJailError),
+    #[Display("Jail is running")]
+    Running,
+    TryIntoJailIdError(TryIntoJailIdError),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,7 +102,57 @@ pub struct JailZone<T> {
 }
 
 impl<'a> JailZone<&'a Zone> {
-    pub(crate) fn volume(&self) -> Result<ZoneVolume<&Zone>, ReadZoneConfigurationError> {
+    fn root_directory_path(&self) -> PathBuf {
+        self.zone.paths().root_directory()
+    }
+
+    fn jail_name(&self) -> JailName {
+        JailName::new(self.zone.identifier().to_string())
+    }
+
+    fn jail_id(&self) -> Result<Option<JailId>, TryIntoJailIdError> {
+        self.jail_name().try_into()
+    }
+
+    fn jail_parameters(&self) -> Vec<JailParameter> {
+        vec![
+            JailParameter::new("persist", "true"),
+            JailParameter::new("name", self.jail_name().to_string()),
+            JailParameter::new("path", self.root_directory_path().display().to_string()),
+        ]
+    }
+
+    fn hold_jail<F, E>(&self, function: F) -> Result<(), E>
+    where
+        F: FnOnce(&Jail) -> Result<(), E>,
+        E: From<CreateJailError> + From<DestroyJailError>,
+    {
+        let jail = Jail::create(self.jail_parameters())?;
+
+        let result = function(&jail);
+
+        jail.destroy()?;
+
+        result
+    }
+
+    fn execute<E>(&self, jail: &Jail, step: &JailZoneConfigurationStep<'a>) -> Result<(), E>
+    where
+        E: From<ExecuteJailError>,
+    {
+        jail.execute(
+            step.program(),
+            &step.arguments().as_ref().cloned().unwrap_or_default(),
+            step.environment_variables()
+                .as_ref()
+                .cloned()
+                .unwrap_or_default(),
+        )?;
+
+        Ok(())
+    }
+
+    pub(super) fn volume(&self) -> Result<ZoneVolume<&Zone>, ReadZoneConfigurationError> {
         let reader = self.zone.configuration().reader()?;
 
         let jail = match reader.r#type() {
@@ -98,7 +173,7 @@ impl<'a> JailZone<&'a Zone> {
         }
     }
 
-    pub(crate) fn create(&self) -> Result<(), CreateJailZoneError> {
+    pub(super) fn create(&self) -> Result<(), CreateJailZoneError> {
         let volume = self.volume()?;
         volume.create()?;
 
@@ -113,24 +188,91 @@ impl<'a> JailZone<&'a Zone> {
             FromHandler::handle(from, &volume.root_directory_path())?;
         }
 
-        Ok(())
-    }
+        self.hold_jail::<_, CreateJailZoneError>(|handle| {
+            for step in jail.create_steps() {
+                self.execute::<CreateJailZoneError>(handle, &step)?;
+            }
 
-    pub(crate) fn start(&self) -> Result<(), StartJailZoneError> {
-        Ok(())
-    }
-
-    pub(crate) fn stop(&self) -> Result<(), StopJailZoneError> {
-        Ok(())
-    }
-
-    pub(crate) fn destroy(&self) -> Result<(), DestroyJailZoneError> {
-        self.volume()?.destroy()?;
+            Ok(())
+        })?;
 
         Ok(())
     }
 
-    pub(crate) fn send(
+    pub(super) fn start(&self) -> Result<(), StartJailZoneError> {
+        if self.jail_id()?.is_some() {
+            return Err(StartJailZoneError::AlreadyRunning);
+        }
+
+        let reader = self.zone.configuration().reader()?;
+
+        let jail = match reader.r#type() {
+            ZoneConfigurationTypeReader::Jail(jail) => jail,
+            ZoneConfigurationTypeReader::Chroot(_chroot) => unreachable!(),
+        };
+
+        let handle = Jail::create(self.jail_parameters())?;
+
+        for step in jail.start_steps() {
+            self.execute::<StartJailZoneError>(&handle, &step)?;
+        }
+
+        Ok(())
+    }
+
+    pub(super) fn stop(&self) -> Result<(), StopJailZoneError> {
+        let jail_id = match self.jail_id()? {
+            Some(jail_id) => jail_id,
+            None => return Err(StopJailZoneError::NotRunning),
+        };
+
+        let handle = match Jail::open(jail_id) {
+            Some(jail) => jail,
+            None => return Err(StopJailZoneError::NotRunning),
+        };
+
+        let reader = self.zone.configuration().reader()?;
+
+        let jail = match reader.r#type() {
+            ZoneConfigurationTypeReader::Jail(jail) => jail,
+            ZoneConfigurationTypeReader::Chroot(_chroot) => unreachable!(),
+        };
+
+        for step in jail.stop_steps() {
+            self.execute::<StopJailZoneError>(&handle, &step)?;
+        }
+
+        Ok(())
+    }
+
+    pub(super) fn destroy(&self) -> Result<(), DestroyJailZoneError> {
+        if self.jail_id()?.is_some() {
+            return Err(DestroyJailZoneError::Running);
+        }
+
+        let volume = self.volume()?;
+
+        let reader = self.zone.configuration().reader()?;
+
+        let jail = match reader.r#type() {
+            ZoneConfigurationTypeReader::Jail(jail) => jail,
+            ZoneConfigurationTypeReader::Chroot(_chroot) => unreachable!(),
+        };
+
+        self.hold_jail::<_, DestroyJailZoneError>(|handle| {
+            for step in jail.destroy_steps() {
+                self.execute::<DestroyJailZoneError>(handle, &step)?;
+            }
+
+            Ok(())
+        })?;
+
+        volume.destroy()?;
+
+        Ok(())
+    }
+
+    pub(super) fn send(
         &self,
         writer: &mut ZoneTransmissionWriter,
     ) -> Result<(), SendJailZoneError> {
@@ -139,7 +281,7 @@ impl<'a> JailZone<&'a Zone> {
         Ok(())
     }
 
-    pub(crate) fn receive(
+    pub(super) fn receive(
         zone: &'a Zone,
         reader: &mut ZoneTransmissionReader,
     ) -> Result<Self, ReceiveJailZoneError> {
@@ -148,7 +290,7 @@ impl<'a> JailZone<&'a Zone> {
         Ok(Self::new(zone))
     }
 
-    pub(crate) fn cleanup(&self) -> Result<(), CleanupJailZoneError> {
+    pub(super) fn cleanup(&self) -> Result<(), CleanupJailZoneError> {
         self.volume()?.cleanup()?;
 
         Ok(())
