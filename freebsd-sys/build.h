3bc2fca1102a690f2d498c2c74d8e0a93a269903
Justus Flerlage <justus@sutsuj.com>
2023-05-29 00:25:27 +0200

    Add DeviceFileSystem

--- freebsd-sys/build.h
+++ freebsd-sys/build.h
@@ -2,3 +2,9 @@
 #include <sys/queue.h>
 #include <fs/devfs/devfs.h>
 #include <elf-hints.h>
+
+// TODO remove usage once bindgen supports proper #define constant handling
+const int int_DRA_BACTS = DRA_BACTS;
+const int int_DRC_PATHPTRN = DRC_PATHPTRN;
+const int int_DRB_HIDE = DRB_HIDE;
+const int int_DRB_UNHIDE = DRB_UNHIDE;
---
+++ freebsd/src/kernel/file_system/device.rs
@@ -0,0 +1,147 @@
+use freebsd_sys::{
+    devfs_rule, devfsio_rapply, int_DRA_BACTS, int_DRB_HIDE, int_DRB_UNHIDE, int_DRC_PATHPTRN,
+    DEVFS_MAGIC,
+};
+use nix::errno::Errno;
+use nix::fcntl::open;
+use nix::fcntl::OFlag;
+use nix::sys::stat::Mode;
+use nix::unistd::close;
+use std::cmp::min;
+use std::ffi::CString;
+use std::os::fd::RawFd;
+use std::path::Path;
+use std::ptr::addr_of_mut;
+use std::ptr::copy_nonoverlapping;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct DeviceFileSystemRules<'a> {
+    file_system: &'a DeviceFileSystem,
+}
+
+impl<'a> DeviceFileSystemRules<'a> {
+    fn new(file_system: &'a DeviceFileSystem) -> Self {
+        Self { file_system }
+    }
+
+    pub fn hide(&self, pattern: &str) -> Result<(), Errno> {
+        let string = match CString::new(pattern) {
+            Err(_e) => unreachable!(),
+            Ok(string) => string,
+        };
+
+        let mut rule = devfs_rule {
+            dr_magic: DEVFS_MAGIC,
+            dr_id: 0,
+            dr_icond: int_DRC_PATHPTRN,
+            dr_dswflags: 0,
+            dr_pathptrn: [0; 200],
+            dr_iacts: int_DRA_BACTS,
+            dr_bacts: int_DRB_HIDE,
+            dr_uid: 0,
+            dr_gid: 0,
+            dr_mode: 0,
+            dr_incset: 0,
+        };
+
+        unsafe {
+            copy_nonoverlapping(
+                string.as_ptr(),
+                rule.dr_pathptrn.as_mut_ptr(),
+                min(rule.dr_pathptrn.len(), string.as_bytes_with_nul().len()),
+            )
+        }
+
+        let result = unsafe { devfsio_rapply(self.file_system.fd, addr_of_mut!(rule)) }?;
+
+        if result > 0 {
+            return Err(Errno::from_i32(result));
+        }
+
+        Ok(())
+    }
+
+    pub fn unhide(&self, pattern: &str) -> Result<(), Errno> {
+        let string = match CString::new(pattern) {
+            Err(_e) => unreachable!(),
+            Ok(string) => string,
+        };
+
+        let mut rule = devfs_rule {
+            dr_magic: DEVFS_MAGIC,
+            dr_id: 0,
+            dr_icond: int_DRC_PATHPTRN,
+            dr_dswflags: 0,
+            dr_pathptrn: [0; 200],
+            dr_iacts: int_DRA_BACTS,
+            dr_bacts: int_DRB_UNHIDE,
+            dr_uid: 0,
+            dr_gid: 0,
+            dr_mode: 0,
+            dr_incset: 0,
+        };
+
+        unsafe {
+            copy_nonoverlapping(
+                string.as_ptr(),
+                rule.dr_pathptrn.as_mut_ptr(),
+                min(rule.dr_pathptrn.len(), string.as_bytes_with_nul().len()),
+            )
+        }
+
+        let result = unsafe { devfsio_rapply(self.file_system.fd, addr_of_mut!(rule)) }?;
+
+        if result > 0 {
+            return Err(Errno::from_i32(result));
+        }
+
+        Ok(())
+    }
+
+    pub fn unhide_all(&self) -> Result<(), Errno> {
+        self.unhide("*")
+    }
+
+    pub fn hide_all(&self) -> Result<(), Errno> {
+        self.hide("*")
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct DeviceFileSystem {
+    closed: bool,
+    fd: RawFd,
+}
+
+impl Drop for DeviceFileSystem {
+    fn drop(&mut self) {
+        if !self.closed {
+            let _ = close(self.fd);
+        }
+    }
+}
+
+impl DeviceFileSystem {
+    pub fn open<T>(path: T) -> Result<Self, Errno>
+    where
+        T: AsRef<Path>,
+    {
+        Ok(Self {
+            fd: open(path.as_ref(), OFlag::O_RDONLY, Mode::empty())?,
+            closed: false,
+        })
+    }
+
+    pub fn close(mut self) -> Result<(), Errno> {
+        self.closed = true;
+        close(self.fd)?;
+
+        Ok(())
+    }
+
+    pub fn rules(&self) -> DeviceFileSystemRules<'_> {
+        DeviceFileSystemRules::new(self)
+    }
+}
